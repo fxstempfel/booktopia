73a9874a13284d585e887c2e229d60ab58d3c245
Fxstempfel <fxstempfel@gmail.com>
2020-05-08 17:31:31 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Booktopia',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Text(
+            'Ta bibliothèque',
+          ),
+        ),
+        floatingActionButton: SpeedDial(
+          // TODO custom icons
+          animatedIcon: AnimatedIcons.menu_close,
+          children: [
+            SpeedDialChild(child: Icon(Icons.search), label: 'Rechercher'),
+            SpeedDialChild(
+              child: Icon(Icons.camera_alt),
+              label: 'Scanner code-barres',
+            ),
+            SpeedDialChild(child: Icon(Icons.file_upload), label: 'Importer')
+          ],
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
