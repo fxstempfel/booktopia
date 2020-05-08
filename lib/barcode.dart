3e369769130d8f46e4bdf6b8a90ca4fe72e778fa
Fxstempfel <fxstempfel@gmail.com>
2020-05-08 18:18:50 +0200

    Scanning barcodes work

--- lib/barcode.dart
+++ lib/barcode.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
+// TODO this could be a single function (scanBarcodeStream()) instead of a full page
 
 class BarcodeScanner extends StatefulWidget {
   @override
@@ -9,24 +10,33 @@ class BarcodeScanner extends StatefulWidget {
 }
 
 class BarcodeScannerState extends State<BarcodeScanner> {
+  var barcodes = <String>[];
+
   void scanBarcodeStream() async {
     FlutterBarcodeScanner.getBarcodeStreamReceiver(
-        "#ff6666", "Cancel", true, ScanMode.BARCODE)
-        .listen((barcode) => print(barcode));
+            "#ff6666", "Cancel", false, ScanMode.BARCODE)
+        .listen((barcode) {
+      setState(() {
+        barcodes.add(barcode);
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(title: const Text('Barcode scan')),
+            appBar: AppBar(title: const Text('Scanner code barres')),
             body: Builder(builder: (BuildContext context) {
-              return Container(
-                  alignment: Alignment.center,
-                  child: FlatButton(
-                    child: Text('Scanner'),
-                    onPressed: scanBarcodeStream,
-                  ));
+              return Column(children: [
+                Text('${barcodes.join(', ')}'),
+                Container(
+                    alignment: Alignment.center,
+                    child: FlatButton(
+                      child: Text('Scanner'),
+                      onPressed: scanBarcodeStream,
+                    ))
+              ]);
             })));
   }
 }
